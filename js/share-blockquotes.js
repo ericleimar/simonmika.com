5ecf8d13f17ae45b309e0ad7045c916945b8b099
Zammu Web <zammuweb@zammu.in>
2016-10-06 05:27:22 -0400

    build e54ca043

---
+++ js/share-blockquotes.js
@@ -0,0 +1,16 @@
+var quotes = document.getElementsByTagName("blockquote")
+for (var i = 0; i < quotes.length; i++){
+	var destinations = {
+//		googleplus: "https://plus.google.com/share?url=" + encodeURIComponent(document.URL),
+		facebook: "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(document.URL),
+		linkedin: "https://www.linkedin.com/shareArticle?mini=true&url=" + encodeURIComponent(document.URL) + "&title=" + encodeURIComponent(document.title) + "&summary=" + encodeURIComponent(quotes[i].textContent.trim()) + "&source=Simon says",
+		twitter: "https://twitter.com/intent/tweet?source=webclient&text=" + encodeURIComponent(quotes[i].textContent.trim() + " via " + document.URL)
+	}
+	for (destination in destinations) {
+		var link = document.createElement("a")
+		link.href = destinations[destination]
+		link.target = "_blank"
+		link.className = "share " + destination
+		quotes[i].insertBefore(link, quotes[i].firstChild)
+	}
+}
